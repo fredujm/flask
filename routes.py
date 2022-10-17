42e96b0580c4155856afb9ad8a62c9ec2a3205e0
frederic <frederic@EMPROA129236.local>
2022-10-17 16:50:07 +0200

    suites

--- routes.py
+++ routes.py
@@ -17,3 +17,15 @@ def list_muns():
     results = Municipality.query.order_by(Municipality.mun_name.asc()).paginate(page=page, per_page=45)
     return render_template('list_mun.html', muns=results)
 
+@app.route("/region/<int:region_id>", methods=['GET'])
+def getRegion(region_id):
+    results = Department.query.filter(Department.region_id == region_id).all()
+    print(len(results))
+    return render_template('region.html', depts=results)
+
+@app.route("/dept/<string:dept_id>", methods=['GET'])
+def getDept(dept_id):
+    page = request.args.get('page', 1, type=int)
+    dept = Department.query.filter(Department.dept_id == dept_id).first()
+    results = Municipality.query.filter(Municipality.dept_id == dept_id).paginate(page=page, per_page=45)
+    return render_template('dept.html', dept=dept, municipalities=results)
